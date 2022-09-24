19e488a48c041d80291335f2f2972bf4016bc660
msxim_m <maximmaksimchyk@gmail.com>
2022-09-24 19:18:50 +0300

    Done task 2

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 { 
-	
+	cout << "\"Kraj lukomor’a dub zelenyj," <<endl; 
+	cout << "\aI zolotyj lancyuh na nim: a" << endl;
+	cout << "hodnya, shonochi kit uchenyj" << endl;
+	cout << "Na lancyuhu kruzhl’aje tim\"" << endl;
 	return 0;
 }
